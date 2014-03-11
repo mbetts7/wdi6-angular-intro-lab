dd78dd438528d603893c80b2271b19c008ad6384
mbetts7 <mbetts7@gmail.com>
2014-03-10 17:14:50 -0700

    can add books and total increments

--- app/books_controller.js
+++ app/books_controller.js
@@ -5,7 +5,23 @@ bookly.bookApp = angular.module("bookApp", []);
 bookly.BooksController = function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
   $scope.books = books;
+  $scope.cartBooks = [];
+  $scope.total = 0;
+
+  $scope.selects = ['author', 'title', 'price'];
+  $scope.select = 'title';
 
   //Code to manage cart goes here
+  $scope.addCart = function(index) {
+    var book = _.find(books, function(b) {
+      return b.id === index;
+    });
+
+    console.log(book.id);
+
+    $scope.cartBooks.push(book);
+    $scope.total += book.price;
+
+  };
 
 };
